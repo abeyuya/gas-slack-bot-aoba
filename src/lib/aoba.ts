714fac8c3ab0cb36dcc972dc9965f9dabc857dfa
abeyuya <yuya.abe.0525@gmail.com>
2018-06-19 00:12:37 +0900

    add hook words and response

--- src/lib/aoba.ts
+++ src/lib/aoba.ts
@@ -1,5 +1,8 @@
 
-import { IBot } from "./slack";
+import { ISlackOutgoingWebhookParams } from "../entrypoint/webhook";
+import { getDialogueMessage } from "./docomo_zatsudan";
+import { IAttachment, IBot, postAsAoba } from "./slack";
+import { randomPickup } from "./util";
 
 // tslint:disable
 const aobaIconList = [
@@ -21,4 +24,70 @@ const todayAobaIcon = (): string => {
 export const aobaBot: IBot = {
   username: "青葉",
   icon_url: todayAobaIcon(),
+};
+
+export const triggerAobaBotFromSlack = (param: ISlackOutgoingWebhookParams) => {
+
+  switch (param.trigger_word) {
+    case "@aoba":
+    case "U3S3FR23F": {
+      execZatsudan(param);
+      break;
+    }
+
+    case ":mo:": {
+      execMo(param);
+      break;
+    }
+
+    case "お疲れ":
+    case "落ちます":
+    case "おつかれ": {
+      execOtsukare(param);
+      break;
+    }
+  }
+};
+
+const execZatsudan = (param: ISlackOutgoingWebhookParams) => {
+  const { text, user_name, channel_id } = param;
+  const receivedMessage = text.replace("@aoba", "").trim();
+  const responseMessage = getDialogueMessage(user_name, receivedMessage);
+
+  postAsAoba(channel_id, responseMessage);
+};
+
+const execMo = (param: ISlackOutgoingWebhookParams) => {
+  const currentHour = new Date().getHours();
+  if (currentHour < 18) { return; }
+  const { channel_id } = param;
+  const attachments: IAttachment[] = [
+    {
+      pretext: "",
+      image_url: "http://livedoor.blogimg.jp/bmaysu/imgs/1/9/192424ff.png",
+    },
+  ];
+  postAsAoba(channel_id, "", attachments);
+};
+
+const otsukareImageList = [
+  "https://pbs.twimg.com/media/CCT9-y2UMAAtQMg.jpg",
+  "http://blog.oukasoft.com/wp-content/uploads/90b5e937575ba81a447c63fdabd0fb87.jpg",
+];
+
+const execOtsukare = (param: ISlackOutgoingWebhookParams) => {
+  const { channel_id, user_name, text } = param;
+  if (user_name === "slackbot" ||
+    text.indexOf("お疲れ様です") === 0 ||
+    text.indexOf("おつかれさまです") === 0
+  ) { return; }
+
+  const imgUrl = randomPickup(otsukareImageList, 1)[0];
+  const attachments: IAttachment[] = [
+    {
+      pretext: "",
+      image_url: imgUrl,
+    },
+  ];
+  postAsAoba(channel_id, "おつかれさまでしたー！", attachments);
 };
