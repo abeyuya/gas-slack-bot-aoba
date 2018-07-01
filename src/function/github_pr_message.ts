04b06d2f893ca4c9defd58a0e9e2a6099e3f8d0a
abeyuya <yuya.abe.0525@gmail.com>
2018-07-01 16:12:49 +0900

    refactoring

--- src/function/github_pr_message.ts
+++ src/function/github_pr_message.ts
@@ -2,10 +2,10 @@
 import "../lib/polyfill";
 import { IAssignedInfo, IAllUserAssignedPrRepo } from "../lib/github";
 
-export const buildGithubPrMessage = (assignedInfo: IAssignedInfo[]) => {
+const buildPrInfoMarkDown = (assignedInfo: IAssignedInfo[]) => {
   const body = assignedInfo.map((info) => {
     return [
-      `# ${info.orgName}`,
+      `## ${info.orgName}`,
       info.assignedPrRepos.map((repo) => {
         return [
           `  - ${repo.repoName}`,
@@ -20,9 +20,13 @@ export const buildGithubPrMessage = (assignedInfo: IAssignedInfo[]) => {
     ].join("\n");
   }).join("\n");
 
+  return body;
+};
+
+export const buildGithubPrMessage = (assignedInfo: IAssignedInfo[]) => {
   const message = [
     "```",
-    body,
+    buildPrInfoMarkDown(assignedInfo),
     "```",
   ].join("\n");
 
@@ -34,22 +38,7 @@ export const buildGithubPrMessageForAllUser = (allUserAssignedInfo: IAllUserAssi
   const body = allUserAssignedInfo.map((allInfo) => {
     return [
       `# ${allInfo.username}`,
-      allInfo.assignedInfo.map((info) => {
-        return [
-          `## ${info.orgName}`,
-          info.assignedPrRepos.map((repo) => {
-            return [
-              `  - ${repo.repoName}`,
-              repo.assignedPrs.map((pr) => {
-                return [
-                  `    - ${pr.title}`,
-                  `      - ${pr.url}`,
-                ].join("\n");
-              }).join("\n"),
-            ].join("\n");
-          }).join("\n"),
-        ].join("\n");
-      }).join("\n"),
+      buildPrInfoMarkDown(allInfo.assignedInfo),
       "",
     ].join("\n");
   }).join("\n");
